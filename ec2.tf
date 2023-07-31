5987792fa9b76667ecc6f9770f8ee41e68d21f1d
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2023-07-31 12:01:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -30,7 +30,7 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-# create subnets in different availability zones
+# create subnets in different availability zoness
 resource "aws_subnet" "subnet_az1" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
