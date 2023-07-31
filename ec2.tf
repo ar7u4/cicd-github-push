b5da2acb18586017325aa09e17bb973c037f4794
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2023-07-31 11:47:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -32,6 +32,17 @@ resource "aws_default_vpc" "default_vpc" {
   }
 }
 
+# use data source to get all available availability zones in the region
+data "aws_availability_zones" "available_zones" {}
+
+# create default subnet if one does not exist
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+
+  tags = {
+    Name = "default subnet"
+  }
+}
 
 # use data source to get all avalablility zones in region
 data "aws_availability_zones" "available_zones" {}
@@ -98,28 +109,51 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-#adding loadbalancing
-resource "aws_elb" "techmax_elb" {
-  name = "techmax-elb"
-  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-  security_groups = [aws_security_group.ec2_security_group.id]
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    load_balancer_port = 80
-    protocol = "http"
-  }
-  health_check {
-    target = "HTTP:80/"
-    interval = 300
-    timeout = 5
-    healthy_threshold = 2
-    unhealthy_threshold = 3
+# launch the ec2 instance and install website
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  user_data              = file("install_techmax.sh")
+
+  tags = {
+    Name = "techmax server"
   }
 }
 
-output "elb_dns_url" {
-  value = aws_elb.techmax_elb.dns_name
+# create the load balancer
+resource "aws_lb" "my_load_balancer" {
+  name               = "my-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ec2_security_group.id]
+  subnets            = [aws_default_subnet.default_az1.id]
+
+  tags = {
+    Name = "My Load Balancer"
+  }
+}
+
+# create the target group for the load balancer
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "My Target Group"
+  }
 }
 
 # launch the ec2 instance and install website
@@ -139,4 +173,9 @@ resource "aws_instance" "ec2_instance" {
 # print the url of the server
 output "ec2_public_ipv4_url" {
   value = join("", ["http://", aws_instance.ec2_instance.public_ip])
+}
+
+# print the URL of the load balancer
+output "load_balancer_dns" {
+  value = aws_lb.my_load_balancer.dns_name
 }
